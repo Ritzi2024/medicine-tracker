b550a20415ca8b0c0a4a7f5d8825c8e58a80a19b
Ritu <sadhanaduraisamy@ymail.com>
2025-04-19 17:43:23 +0530

    Tresting githubpages again

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { ManageMedicineListComponent } from './manage-medicine-list/manage-medic
 export const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
     { path: 'manage-medicine-list', component: ManageMedicineListComponent },
-    { path: '',   redirectTo: '/dashboard', pathMatch: 'full' }, // redirect to `first-component`
+    { path: '',   redirectTo: '/manage-medicine-list', pathMatch: 'full' }, // redirect to `first-component`
     { path: '**', component: DashboardComponent }
 
 ];
